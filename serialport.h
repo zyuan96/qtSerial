757ae8cb6ad717d4e5685d6327edf79d702b62ff
zyuan96 <20154646@stu.neu.edu.cn>
2018-03-11 21:51:56 +0800

    修改readme

--- serialport.h
+++ serialport.h
@@ -1,4 +1,15 @@
 #ifndef SERIALPORT_H
 #define SERIALPORT_H
+/*
+ * 建立一个单独的串口类来进行串口操作
+ * ------------------------------
+ */
+
+
+class SerailPort
+{
+public:
+    SerailPort() {}
+};
 
 #endif // SERIALPORT_H
